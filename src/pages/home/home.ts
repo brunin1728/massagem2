fcec0f8ea76d3506fcc011c56d97063a20ea3cf5
Bruno Carvalho <brunocarvalho.info@gmail.com>
2019-01-22 14:01:32 -0200

    as

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private iab: InAppBrowser,
+    splashScreen: SplashScreen
+    ) {
 
+      splashScreen.hide();
+this.abrir();
   }
+
+
+
+abrir(){
+  const options: InAppBrowserOptions = {
+    zoom: 'no',
+    location: 'no'
+  }
+  const browser = this.iab.create('http://www.massagista.nelsonmoraes.com.br/', '_blank', options);
+}
+
 
 }
